c4a3f06b2b7d0b8d8691604bcdd6488a174ec3f7
semvis123 <semvisscher10@gmail.com>
2024-05-02 20:12:28 +0200

    change websocket url

--- realtime.go
+++ realtime.go
@@ -55,7 +55,7 @@ func (r *Realtime) Channel(topic string) *Channel {
 	websocketUrl := r.client.BaseURL
 	websocketUrl = strings.Replace(websocketUrl, "https://", "wss://", 1)
 	websocketUrl = strings.Replace(websocketUrl, "http://", "ws://", 1)
-	websocketUrl = fmt.Sprintf("%s/ws", websocketUrl)
+	websocketUrl = fmt.Sprintf("%s/realtime/v1/websocket", websocketUrl)
 	return &Channel{topic, websocketUrl, "http://localhost/", nil, nil, false, make(chan struct{})}
 }
 
